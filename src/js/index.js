c030eb5994a6279f87bd1f37faf4ffe344b6af7a
mazilanx <mazilanx@gmail.com>
2020-03-16 18:23:57 +0300

    fixed some bugs

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import bootstrap from "bootstrap";
 import onepageScroll from './jquery.onepage-scroll.js';
 
 
-$(document).ready(function(){
+$(document).ready(function () {
   if (window.location.hash) {
     window.location.hash = '';
   }
@@ -55,7 +55,7 @@ $(document).ready(function(){
           break;
       }
     },
-    afterMove: function(index) {
+    afterMove: function (index) {
       switch (index) {
         case 1:
           break;
@@ -77,91 +77,74 @@ $(document).ready(function(){
           setTimeout(function () {
             (function iterate(i) {
               var arr = divideString(mainText1);
-              setTimeout(function() {
+              setTimeout(function () {
                 currentString += arr[i];
                 $('.js-print').html(currentString)
                 if (i < arr.length - 1) {
                   iterate(i + 1);
+                } else {
+                  $('.js-print').append('<span class="red js-red-1"> </span>');
+                  (function iterate(i) {
+                    var arr2 = divideString(mainText2);
+                    setTimeout(function () {
+                      currentString2 += arr2[i];
+                      $('.js-red-1').html(currentString2);
+                      if (i < arr2.length - 1) {
+                        iterate(i + 1);
+                      } else {
+                        (function iterate(i) {
+                          var arr = divideString(mainText3);
+                          setTimeout(function () {
+                            var oldHtml = $('.js-print').html();
+                            $('.js-print').html(oldHtml + arr[i])
+                            if (i < arr.length - 1) {
+                              iterate(i + 1);
+                            } else {
+                              $('.js-print').append('<span class="red js-red-2"></span>');
+                              (function iterate(i) {
+                                var arr4 = divideString(mainText4);
+                                setTimeout(function () {
+                                  currentString4 += arr4[i];
+                                  $('.js-red-2').html(currentString4);
+                                  if (i < arr4.length - 1) {
+                                    iterate(i + 1);
+                                  } else {
+                                    (function iterate(i) {
+                                      var arr5 = divideString(mainText5);
+                                      setTimeout(function () {
+                                        var oldHtml = $('.js-print').html();
+                                        $('.js-print').html(oldHtml + arr5[i])
+                                        if (i < arr5.length - 1) {
+                                          iterate(i + 1);
+                                        } else {
+                                          (function iterate(i) {
+                                            var arr6 = divideString(authorText);
+                                            setTimeout(function () {
+                                              var oldHtml = $('.js-author').html();
+                                              $('.js-author').html(oldHtml + arr6[i])
+                                              if (i < arr6.length - 1) {
+                                                iterate(i + 1);
+                                              } else {
+                                                $('.js-author').parent().append('<div class="line-2"></div>')
+                                              }
+                                            }, 40);
+                                          })(0);
+                                        }
+                                      }, 40);
+                                    })(0);
+                                  }
+                                }, 40);
+                              })(0);
+                            }
+                          }, 40)
+                        })(0);
+                      }
+                    }, 40);
+                  })(0);
                 }
               }, 40);
             })(0);
           }, 1200);
-          setTimeout(function () {
-            $('.js-print').append('<span class="red js-red-1"> </span>');
-          }, 2000);
-          setTimeout(function () {
-            (function iterate(i) {
-              var arr = divideString(mainText2);
-              setTimeout(function() {
-                currentString2 += arr[i];
-                $('.js-red-1').html(currentString2);
-                if (i < arr.length - 1) {
-                  iterate(i + 1);
-                }
-              }, 40);
-            })(0);
-          }, 2030);
-
-          setTimeout(function () {
-            (function iterate(i) {
-              var arr = divideString(mainText3);
-              setTimeout(function() {
-                var oldHtml = $('.js-print').html();
-                $('.js-print').html(oldHtml + arr[i])
-                if (i < arr.length - 1) {
-                  iterate(i + 1);
-                }
-              }, 40);
-            })(0);
-          }, 2500);
-
-
-          setTimeout(function () {
-            $('.js-print').append('<span class="red js-red-2"></span>');
-          }, 2900);
-          //
-          setTimeout(function () {
-            (function iterate(i) {
-              var arr = divideString(mainText4);
-              setTimeout(function() {
-                currentString4 += arr[i];
-                $('.js-red-2').html(currentString4);
-                if (i < arr.length - 1) {
-                  iterate(i + 1);
-                }
-              }, 40);
-            })(0);
-          }, 2940);
-
-          setTimeout(function () {
-            (function iterate(i) {
-              var arr = divideString(mainText5);
-              setTimeout(function() {
-                var oldHtml = $('.js-print').html();
-                $('.js-print').html(oldHtml + arr[i])
-                if (i < arr.length - 1) {
-                  iterate(i + 1);
-                }
-              }, 40);
-            })(0);
-          }, 3580);
-
-          setTimeout(function () {
-           (function iterate(i) {
-             var arr = divideString(authorText);
-             setTimeout(function() {
-               var oldHtml = $('.js-author').html();
-               $('.js-author').html(oldHtml + arr[i])
-               if (i < arr.length - 1) {
-                 iterate(i + 1);
-               }
-             }, 40);
-           })(0);
-          }, 5750);
-          setTimeout(function () {
-            $('.js-author').parent().append('<div class="line-2"></div>')
-          }, 6400);
-
           break;
         case 3:
           if (donePage.isDone3 === true) {
@@ -169,6 +152,8 @@ $(document).ready(function(){
           }
           donePage.isDone3 = true;
           $('.js-title-page3').append('<div class="underline-title-creator"></div>');
+          $('.js-gif-mobile').attr("src","./../../../img/bg-3-rotate.gif");
+          $('.js-gif').attr("src","./../../../img/bg-3.gif");
           break;
         case 4:
           if (donePage.isDone4 === true) {
@@ -198,6 +183,6 @@ $(document).ready(function(){
 });
 
 
-function divideString (string) {
+function divideString(string) {
   return string.split('');
 }
